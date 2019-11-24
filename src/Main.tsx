d80ee77d88b42094776a22d4cd1aacc7857c2530
Ahmed ElSenousi <asenousy@gmail.com>
2019-11-24 00:52:18 +0000

    storeReview

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,21 +1,49 @@
-import React, { memo } from "react";
-import { StyleSheet, View, Platform, StatusBar } from "react-native";
+import React, { PureComponent } from "react";
+import { StyleSheet, View, Platform, StatusBar, AppState } from "react-native";
 import Constants from "expo-constants";
 import { AdMobBanner } from "expo-ads-admob";
 import Navigator from "./Navigator";
 import configs from "../configs.json";
+import storeReview from "./storeReview";
 
 const AD_UNIT_ID = __DEV__
   ? configs.testAdUnitID
   : configs[Platform.OS].adUnitID;
 
-const Main = () => (
-  <View style={styles.wrapper}>
-    <StatusBar barStyle="dark-content" />
-    <AdMobBanner adUnitID={AD_UNIT_ID} servePersonalizedAds />
-    <Navigator />
-  </View>
-);
+class Main extends PureComponent {
+  state = {
+    appState: AppState.currentState
+  };
+
+  componentDidMount() {
+    storeReview();
+    AppState.addEventListener("change", this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this.handleAppStateChange);
+  }
+
+  handleAppStateChange = nextAppState => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      storeReview();
+    }
+    this.setState({ appState: nextAppState });
+  };
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <StatusBar barStyle="dark-content" />
+        <AdMobBanner adUnitID={AD_UNIT_ID} servePersonalizedAds />
+        <Navigator />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -25,4 +53,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default memo(Main);
+export default Main;
---
+++ src/storeReview.ts
@@ -0,0 +1,57 @@
+import { AsyncStorage, Alert } from "react-native";
+import * as StoreReview from "expo-store-review";
+
+const YEAR = 12 * 30 * 24 * 60 * 60 * 1000;
+const MONTH = 30 * 24 * 60 * 60 * 1000;
+// const YEAR = 5 * 60 * 1000;
+// const MONTH = 1 * 60 * 1000;
+const LAST_REVIEW_DATE = "lastReviewDate";
+
+async function storeDate(overrideDate?: number) {
+  const date = overrideDate || Date.now();
+  try {
+    await AsyncStorage.setItem(LAST_REVIEW_DATE, date.toString());
+  } catch (error) {
+    if (__DEV__) throw error;
+  }
+}
+
+async function retrieveDate() {
+  try {
+    const value = await AsyncStorage.getItem(LAST_REVIEW_DATE);
+    return +value;
+  } catch (error) {
+    if (__DEV__) throw error;
+  }
+}
+
+const myReview = () =>
+  Alert.alert(
+    "Review the App",
+    "Enjoying the app ? please support me with a nice review",
+    [
+      {
+        text: "review",
+        onPress: StoreReview.requestReview
+      },
+      { text: "cancel" }
+    ]
+  );
+
+async function review() {
+  try {
+    const lastReviewDate = await retrieveDate();
+    if (lastReviewDate) {
+      if (Date.now() - lastReviewDate > YEAR) {
+        StoreReview.isSupported() ? StoreReview.requestReview() : myReview();
+        await storeDate();
+      }
+    } else {
+      await storeDate(Date.now() - (YEAR - MONTH));
+    }
+  } catch (error) {
+    if (__DEV__) throw error;
+  }
+}
+
+export default review;
