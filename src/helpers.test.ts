f8cec994d845421e1dce95e49cc7b35f655da87c
Ahmed ElSenousi <asenousy@gmail.com>
2019-12-01 00:23:09 +0000

    fix test

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,5 +1,5 @@
 jest.mock("react-native", () => ({
-  Dimensions: { get: () => ({ height: 1001 }) }
+  Dimensions: { get: () => ({ height: 1100 }) }
 }));
 
 import { responsive } from "./helpers";
@@ -7,7 +7,7 @@ import { responsive } from "./helpers";
 test("responsive", () => {
   const style = { container: { color: "yellow", width: 10 } };
   expect(responsive(style)).toEqual({
-    container: { color: "yellow", width: 20 }
+    container: { color: "yellow", width: 14 }
   });
-  expect(responsive(20)).toBe(40);
+  expect(responsive(20)).toBe(28);
 });
